8f4a10368265241e0903e6adea690e95a55c951f
albingidenstam-crypto <albin.gidenstam@edu.stenungsund.se>
2026-05-26 15:35:01 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,52 @@
+let aaaaa = 0
+game.setGameOverMessage(true, "GAME OVER!")
+game.splash("", "")
+game.setDialogTextColor(2)
+game.setDialogCursor(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `)
+music.play(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+music.play(music.stringPlayable("D F G E F C D C ", 120), music.PlaybackMode.UntilDone)
+tiles.setWallAt(tiles.getTileLocation(2, 33), true)
+animation.runImageAnimation(
+null,
+[img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `],
+500,
+true
+)
+mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One))
+game.showLongText("", DialogLayout.Bottom)
+aaaaa += 1
+aaaaa += 2
